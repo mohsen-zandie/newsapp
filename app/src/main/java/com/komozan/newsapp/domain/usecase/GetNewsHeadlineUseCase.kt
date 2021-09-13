308d0845c66d4e0e9ae3e1e7842864f1be997b7e
mohsen <mohsen.zandie1@gmail.com>
2021-09-13 15:16:31 +0430

    Adds get news headlines use case functionality

--- app/src/main/java/com/komozan/newsapp/domain/usecase/GetNewsHeadlineUseCase.kt
+++ app/src/main/java/com/komozan/newsapp/domain/usecase/GetNewsHeadlineUseCase.kt
@@ -1,7 +1,11 @@
 package com.komozan.newsapp.domain.usecase
 
+import com.komozan.newsapp.data.model.response.everything.EverythingResponse
+import com.komozan.newsapp.data.util.Resource
 import com.komozan.newsapp.domain.repository.NewsRepository
 
 class GetNewsHeadlineUseCase(private val newsRepository: NewsRepository) {
-
+    suspend fun execute(): Resource<EverythingResponse> {
+        return newsRepository.getNewsHeadlines()
+    }
 }
