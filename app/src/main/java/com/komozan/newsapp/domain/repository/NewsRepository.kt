277c189632202a0b7f014fd48ad9cda4257b959a
mohsen <mohsen.zandie1@gmail.com>
2021-09-13 15:15:51 +0430

    Adds get news headlines function to repository interface

--- app/src/main/java/com/komozan/newsapp/domain/repository/NewsRepository.kt
+++ app/src/main/java/com/komozan/newsapp/domain/repository/NewsRepository.kt
@@ -1,4 +1,8 @@
 package com.komozan.newsapp.domain.repository
 
+import com.komozan.newsapp.data.model.response.everything.EverythingResponse
+import com.komozan.newsapp.data.util.Resource
+
 interface NewsRepository {
+    suspend fun getNewsHeadlines(): Resource<EverythingResponse>
 }
