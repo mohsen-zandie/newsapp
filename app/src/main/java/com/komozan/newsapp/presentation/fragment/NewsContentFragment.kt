1673dfa7c6dbf52c031781e7e1243420e18bd0a3
mohsen <mohsen.zandie1@gmail.com>
2021-09-14 22:02:13 +0430

    Adds web view and load the article url in it

--- app/src/main/java/com/komozan/newsapp/presentation/fragment/NewsContentFragment.kt
+++ app/src/main/java/com/komozan/newsapp/presentation/fragment/NewsContentFragment.kt
@@ -1,18 +1,18 @@
 package com.komozan.newsapp.presentation.fragment
 
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.komozan.newsapp.R
+import com.komozan.newsapp.databinding.FragmentNewsContentBinding
 
 class NewsContentFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
+    private lateinit var binding: FragmentNewsContentBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,11 +20,17 @@ class NewsContentFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_news_content, container, false)
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NewsContentFragment().apply {
-                arguments = Bundle()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentNewsContentBinding.bind(view)
+        val args: NewsContentFragmentArgs by navArgs()
+        val article = args.selectedArticle
+        binding.webView.apply {
+            webViewClient = WebViewClient()
+            if (!TextUtils.isEmpty(article.url)) {
+                loadUrl(article.url)
             }
+        }
     }
+
 }
