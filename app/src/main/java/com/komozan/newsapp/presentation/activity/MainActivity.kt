128721781806ed5dc28f9a2a2e4cbbfb36e89a8c
mohsen <mohsen.zandie1@gmail.com>
2021-09-14 19:54:52 +0430

    Adds view model and view model factory

--- app/src/main/java/com/komozan/newsapp/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/komozan/newsapp/presentation/activity/MainActivity.kt
@@ -2,13 +2,24 @@ package com.komozan.newsapp.presentation.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.komozan.newsapp.R
 import com.komozan.newsapp.databinding.ActivityMainBinding
+import com.komozan.newsapp.presentation.viewmodel.NewsViewModel
+import com.komozan.newsapp.presentation.viewmodel.NewsViewModelFactory
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    @Inject
+    lateinit var factory: NewsViewModelFactory
+    lateinit var viewModel: NewsViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -17,5 +28,6 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bottomNavigation.setupWithNavController(navController)
+        viewModel = ViewModelProvider(this, factory).get(NewsViewModel::class.java)
     }
 }
