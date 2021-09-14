b84ad7ac6d934d346f7d72529e535acfaddecb94
mohsen <mohsen.zandie1@gmail.com>
2021-09-14 18:25:54 +0430

    Adds navigation graph, navigation controller and navigation host

---
+++ app/src/main/java/com/komozan/newsapp/presentation/activity/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.komozan.newsapp.presentation.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.komozan.newsapp.R
+import com.komozan.newsapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigation.setupWithNavController(navController)
+    }
+}
