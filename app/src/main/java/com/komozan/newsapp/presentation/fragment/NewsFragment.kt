bd29671e6f2899e5db598500f08dfe3551d46677
mohsen <mohsen.zandie1@gmail.com>
2021-09-14 19:57:27 +0430

    Adds recycler view, initializes it, get news headline and show result in the recycler view

--- app/src/main/java/com/komozan/newsapp/presentation/fragment/NewsFragment.kt
+++ app/src/main/java/com/komozan/newsapp/presentation/fragment/NewsFragment.kt
@@ -4,14 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.komozan.newsapp.R
+import com.komozan.newsapp.data.util.Resource
+import com.komozan.newsapp.databinding.FragmentNewsBinding
+import com.komozan.newsapp.presentation.activity.MainActivity
+import com.komozan.newsapp.presentation.adapter.NewsAdapter
+import com.komozan.newsapp.presentation.viewmodel.NewsViewModel
 
 class NewsFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
-    }
+    private lateinit var viewModel: NewsViewModel
+    private lateinit var binding: FragmentNewsBinding
+    private lateinit var myAdapter: NewsAdapter
+    private val country = "us"
+    private val page = 1
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,10 +29,56 @@ class NewsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_news, container, false)
     }
 
-    companion object {
-        fun newInstance(param1: String, param2: String) =
-            NewsFragment().apply {
-                arguments = Bundle()
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as MainActivity).viewModel
+        binding = FragmentNewsBinding.bind(view)
+        initRecyclerView()
+        viewNewsList()
     }
+
+    private fun viewNewsList() {
+        viewModel.getNewsHeadlines(country, page)
+        viewModel.newsHeadline.observe(viewLifecycleOwner, { response ->
+            when (response) {
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let {
+                        myAdapter.differ.submitList(it.articles.toList())
+                    }
+                }
+                is Resource.Error -> {
+                    response.data?.let {
+                        showMessage(it.toString())
+                    }
+                }
+                is Resource.Loading -> {
+                    showProgressBar()
+                }
+            }
+        })
+    }
+
+    private fun showMessage(message: String) {
+        Toast.makeText(activity, message, Toast.LENGTH_LONG).show()
+
+    }
+
+    private fun initRecyclerView() {
+        myAdapter = NewsAdapter()
+        binding.recyclerView.apply {
+            adapter = myAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+
+    }
+
+    private fun showProgressBar() {
+        binding.progressBar.visibility = View.VISIBLE
+    }
+
+    private fun hideProgressBar() {
+        binding.progressBar.visibility = View.INVISIBLE
+    }
+
 }
