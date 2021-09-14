be33de6b39c4a455c7a69d97ff9d9f9dcef2f781
mohsen <mohsen.zandie1@gmail.com>
2021-09-14 13:13:17 +0430

    Adds news api service test class

---
+++ app/src/test/java/com/komozan/newsapp/data/NewsAPIServiceTest.kt
@@ -0,0 +1,77 @@
+package com.komozan.newsapp.data
+
+import com.google.common.truth.Truth.assertThat
+import com.komozan.newsapp.data.api.NewsAPIService
+import kotlinx.coroutines.runBlocking
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import okio.buffer
+import okio.source
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class NewsAPIServiceTest {
+    private lateinit var service: NewsAPIService
+    private lateinit var server: MockWebServer
+
+    @Before
+    fun setUp() {
+        server = MockWebServer()
+        service = Retrofit.Builder()
+            .baseUrl(server.url(""))
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(NewsAPIService::class.java)
+    }
+
+    private fun enqueueMockResponse(fileName: String) {
+        val inputStream = javaClass.classLoader!!.getResourceAsStream(fileName)
+        val source = inputStream.source().buffer()
+        val mockResponse = MockResponse()
+        mockResponse.setBody(source.readString(Charsets.UTF_8))
+        server.enqueue(mockResponse)
+    }
+
+    @Test
+    fun getTopHeadlines_sentRequest_receivedExpected() {
+        runBlocking {
+            enqueueMockResponse("newsresponse.json")
+            val responseBody = service.getTopHeadlines("us", 1).body()
+            val request = server.takeRequest()
+            assertThat(responseBody).isNotNull()
+            assertThat(request.path).isEqualTo("/v2/top-headlines?country=us&page=1&apiKey=25739b16ef9f4672bf0f710c358b225e")
+
+        }
+    }
+
+    @Test
+    fun getTopHeadlines_receivedResponse_correctPageSize(){
+        runBlocking {
+            enqueueMockResponse("newsresponse.json")
+            val responseBody = service.getTopHeadlines("us",1).body()
+            val articlesList = responseBody!!.articles
+            assertThat(articlesList.size).isEqualTo(20)
+        }
+    }
+
+    @Test
+    fun getTopHeadlines_receivedResponse_correctContent(){
+        runBlocking {
+            enqueueMockResponse("newsresponse.json")
+            val responseBody = service.getTopHeadlines("us",1).body()
+            val articlesList = responseBody!!.articles
+            val article = articlesList[0]
+            assertThat(article.author).isEqualTo("Nathan Jahnke")
+            assertThat(article.url).isEqualTo("https://www.pff.com/news/fantasy-football-week-1-recap-key-takeaways-las-vegas-raiders-33-27-baltimore-ravens-monday-night-football-2021")
+            assertThat(article.publishedAt).isEqualTo("2021-09-14T04:11:18Z")
+        }
+    }
+
+    @After
+    fun tearDown() {
+        server.shutdown()
+    }
+}
