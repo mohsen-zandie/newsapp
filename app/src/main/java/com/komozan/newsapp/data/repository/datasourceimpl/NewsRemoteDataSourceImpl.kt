7b2e0183933c25a21c54641d32c41d1700e18e5e
mohsen <mohsen.zandie1@gmail.com>
2021-09-14 15:20:21 +0430

    Renames NewRemoteDataSourceImpl.kt to NewsRemoteDataSourceImpl.kt

--- app/src/main/java/com/komozan/newsapp/data/repository/datasourceimpl/NewsRemoteDataSourceImpl.kt
+++ app/src/main/java/com/komozan/newsapp/data/repository/datasourceimpl/NewsRemoteDataSourceImpl.kt
@@ -5,7 +5,7 @@ import com.komozan.newsapp.data.model.response.everything.EverythingResponse
 import com.komozan.newsapp.data.repository.datasource.NewsRemoteDataSource
 import retrofit2.Response
 
-class NewRemoteDataSourceImpl(
+class NewsRemoteDataSourceImpl(
     private val service: NewsAPIService
 ) : NewsRemoteDataSource {
     override suspend fun getTopHeadlines(country: String, page: Int): Response<EverythingResponse> {
