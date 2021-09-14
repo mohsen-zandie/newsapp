eb9a34dcd978853bb76dc244ca435e35ac43d18b
mohsen <mohsen.zandie1@gmail.com>
2021-09-14 11:18:19 +0430

    Adds news repository implementation

---
+++ app/src/main/java/com/komozan/newsapp/data/repository/NewsRepositoryImpl.kt
@@ -0,0 +1,22 @@
+package com.komozan.newsapp.data.repository
+
+import com.komozan.newsapp.data.model.response.everything.EverythingResponse
+import com.komozan.newsapp.data.repository.datasource.NewsRemoteDataSource
+import com.komozan.newsapp.data.util.Resource
+import com.komozan.newsapp.domain.repository.NewsRepository
+import retrofit2.Response
+
+class NewsRepositoryImpl(private val remoteDataSource: NewsRemoteDataSource) : NewsRepository {
+    override suspend fun getNewsHeadlines(): Resource<EverythingResponse> {
+        return responseToResource(remoteDataSource.getTopHeadlines())
+    }
+
+    private fun responseToResource(response: Response<EverythingResponse>): Resource<EverythingResponse> {
+        if(response.isSuccessful){
+            response.body()?.let { result ->
+                return Resource.Success(result)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+}
