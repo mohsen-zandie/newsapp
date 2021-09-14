7698537f9d2683d006629981a3f9e896b6d683d0
mohsen <mohsen.zandie1@gmail.com>
2021-09-14 21:09:12 +0430

    Adds a manual pagination to NewsFragment.kt

--- app/src/main/java/com/komozan/newsapp/presentation/fragment/NewsFragment.kt
+++ app/src/main/java/com/komozan/newsapp/presentation/fragment/NewsFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AbsListView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.komozan.newsapp.R
 import com.komozan.newsapp.data.util.Resource
 import com.komozan.newsapp.databinding.FragmentNewsBinding
@@ -20,7 +22,15 @@ class NewsFragment : Fragment() {
     private lateinit var binding: FragmentNewsBinding
     private lateinit var newsAdapter: NewsAdapter
     private val country = "us"
-    private val page = 1
+    private var page = 1
+    private var isScrolling = false
+    private var isLoading = false
+    private var isLastPage = false
+    private var pages = 0
+
+    companion object {
+        const val DEFAULT_API_PAGE_SIZE = 20
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -46,6 +56,12 @@ class NewsFragment : Fragment() {
                     hideProgressBar()
                     response.data?.let {
                         newsAdapter.differ.submitList(it.articles.toList())
+                        if (it.totalResults % DEFAULT_API_PAGE_SIZE == 0) {
+                            pages = it.totalResults / DEFAULT_API_PAGE_SIZE
+                        } else {
+                            pages = it.totalResults / DEFAULT_API_PAGE_SIZE + 1
+                        }
+                        isLastPage = pages == page
                     }
                 }
                 is Resource.Error -> {
@@ -62,23 +78,52 @@ class NewsFragment : Fragment() {
 
     private fun showMessage(message: String) {
         Toast.makeText(activity, message, Toast.LENGTH_LONG).show()
-
     }
 
     private fun initRecyclerView() {
         binding.recyclerView.apply {
             adapter = newsAdapter
             layoutManager = LinearLayoutManager(activity)
+            addOnScrollListener(this@NewsFragment.onScrollListener)
         }
 
     }
 
     private fun showProgressBar() {
+        isLoading = true
         binding.progressBar.visibility = View.VISIBLE
     }
 
     private fun hideProgressBar() {
+        isLoading = false
         binding.progressBar.visibility = View.INVISIBLE
+    }
+
+    private val onScrollListener = object : RecyclerView.OnScrollListener() {
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            super.onScrollStateChanged(recyclerView, newState)
+            if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
+                isScrolling = true
+            }
+        }
+
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+            val layoutManager = binding.recyclerView.layoutManager as LinearLayoutManager
+            val sizeOfCurrentList = layoutManager.itemCount
+            val sizeOfVisibleItems = layoutManager.childCount
+            val toPosition = layoutManager.findFirstVisibleItemPosition()
+
+            val hasReachedToEnd = toPosition + sizeOfVisibleItems >= sizeOfCurrentList
+
+            val shouldPaginate = !isLoading && !isLastPage && hasReachedToEnd && isScrolling
+            if (shouldPaginate) {
+                page++
+                viewModel.getNewsHeadlines(country, page)
+                isScrolling = false
+            }
+
+        }
     }
 
 }
