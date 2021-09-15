c9290bc67af1a6809036147450a4ab89ebb9a796
mohsen <mohsen.zandie1@gmail.com>
2021-09-15 18:09:29 +0430

    Renames response data model to APIResponse.kt

--- app/src/main/java/com/komozan/newsapp/data/model/response/everything/APIResponse.kt
+++ app/src/main/java/com/komozan/newsapp/data/model/response/everything/APIResponse.kt
@@ -1,6 +1,6 @@
 package com.komozan.newsapp.data.model.response.everything
 
-data class EverythingResponse(
+data class APIResponse(
     val articles: List<Article>,
     val status: String,
     val totalResults: Int
