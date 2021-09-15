c4782e47b82d48ecd1a0bad9210f8c91c3b3711f
mohsen <mohsen.zandie1@gmail.com>
2021-09-15 18:21:04 +0430

    Adds modules for repository and view model factory

--- app/src/main/java/com/komozan/newsapp/presentation/di/FactoryModule.kt
+++ app/src/main/java/com/komozan/newsapp/presentation/di/FactoryModule.kt
@@ -2,6 +2,8 @@ package com.komozan.newsapp.presentation.di
 
 import android.app.Application
 import com.komozan.newsapp.domain.usecase.GetNewsHeadlineUseCase
+import com.komozan.newsapp.domain.usecase.GetSpecifiedNewsAgencyUseCase
+import com.komozan.newsapp.domain.usecase.SaveNewsUseCase
 import com.komozan.newsapp.presentation.viewmodel.NewsViewModelFactory
 import dagger.Module
 import dagger.Provides
@@ -16,8 +18,15 @@ class FactoryModule {
     @Singleton
     fun provideNewsViewModelFactory(
         application: Application,
-        getNewsHeadlineUseCase: GetNewsHeadlineUseCase
+        getNewsHeadlineUseCase: GetNewsHeadlineUseCase,
+        getSpecifiedNewsAgencyUseCase: GetSpecifiedNewsAgencyUseCase,
+        saveNewsUseCase: SaveNewsUseCase
     ): NewsViewModelFactory {
-        return NewsViewModelFactory(application, getNewsHeadlineUseCase)
+        return NewsViewModelFactory(
+            application,
+            getNewsHeadlineUseCase,
+            getSpecifiedNewsAgencyUseCase,
+            saveNewsUseCase
+        )
     }
 }
--- app/src/main/java/com/komozan/newsapp/presentation/di/RepositoryModule.kt
+++ app/src/main/java/com/komozan/newsapp/presentation/di/RepositoryModule.kt
@@ -1,6 +1,7 @@
 package com.komozan.newsapp.presentation.di
 
 import com.komozan.newsapp.data.repository.NewsRepositoryImpl
+import com.komozan.newsapp.data.repository.datasource.NewsDataLocalDataSource
 import com.komozan.newsapp.data.repository.datasource.NewsRemoteDataSource
 import com.komozan.newsapp.domain.repository.NewsRepository
 import dagger.Module
@@ -15,7 +16,7 @@ class RepositoryModule {
 
     @Provides
     @Singleton
-    fun provideNewsRepositoryModule(remoteDataSource: NewsRemoteDataSource): NewsRepository {
-        return NewsRepositoryImpl(remoteDataSource)
+    fun provideNewsRepositoryModule(remoteDataSource: NewsRemoteDataSource, localDataSource: NewsDataLocalDataSource): NewsRepository {
+        return NewsRepositoryImpl(remoteDataSource, localDataSource)
     }
 }
