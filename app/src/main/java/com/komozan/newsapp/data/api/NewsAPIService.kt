438f8be537ae5e5ce8f2858009bc0e7bb3376323
mohsen <mohsen.zandie1@gmail.com>
2021-09-15 18:10:05 +0430

    Adds everything endpoint

--- app/src/main/java/com/komozan/newsapp/data/api/NewsAPIService.kt
+++ app/src/main/java/com/komozan/newsapp/data/api/NewsAPIService.kt
@@ -1,7 +1,7 @@
 package com.komozan.newsapp.data.api
 
 import com.komozan.newsapp.BuildConfig
-import com.komozan.newsapp.data.model.response.everything.EverythingResponse
+import com.komozan.newsapp.data.model.response.everything.APIResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -15,5 +15,13 @@ interface NewsAPIService {
         page: Int,
         @Query("apiKey")
         apiKey: String = BuildConfig.API_KEY
-    ): Response<EverythingResponse>
+    ): Response<APIResponse>
+
+    @GET("v2/everything")
+    suspend fun getSpecifiedNewsAgency(
+        @Query("domains")
+        domain: String,
+        @Query("apiKey")
+        apiKey: String = BuildConfig.API_KEY
+    ): Response<APIResponse>
 }
