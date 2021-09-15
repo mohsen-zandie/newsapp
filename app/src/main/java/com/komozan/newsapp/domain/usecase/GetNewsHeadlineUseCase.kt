a954eeafeaf63b2a29df0a40ec13335e0314b735
mohsen <mohsen.zandie1@gmail.com>
2021-09-15 18:15:30 +0430

    Modifies names in GetNewsHeadlineUseCase.kt and add functions for saving news and getting specified news

--- app/src/main/java/com/komozan/newsapp/domain/usecase/GetNewsHeadlineUseCase.kt
+++ app/src/main/java/com/komozan/newsapp/domain/usecase/GetNewsHeadlineUseCase.kt
@@ -1,11 +1,11 @@
 package com.komozan.newsapp.domain.usecase
 
-import com.komozan.newsapp.data.model.response.everything.EverythingResponse
+import com.komozan.newsapp.data.model.response.everything.APIResponse
 import com.komozan.newsapp.data.util.Resource
 import com.komozan.newsapp.domain.repository.NewsRepository
 
 class GetNewsHeadlineUseCase(private val newsRepository: NewsRepository) {
-    suspend fun execute(country: String, page: Int): Resource<EverythingResponse> {
+    suspend fun execute(country: String, page: Int): Resource<APIResponse> {
         return newsRepository.getNewsHeadlines(country, page)
     }
 }
