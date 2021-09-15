d5550b60abc0fad78ddeac1c517bed8a6bff977e
mohsen <mohsen.zandie1@gmail.com>
2021-09-15 18:21:47 +0430

    Save article when they read in database

--- app/src/main/java/com/komozan/newsapp/presentation/fragment/NewsContentFragment.kt
+++ app/src/main/java/com/komozan/newsapp/presentation/fragment/NewsContentFragment.kt
@@ -1,18 +1,22 @@
 package com.komozan.newsapp.presentation.fragment
 
+import android.net.http.SslError
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.WebViewClient
+import android.webkit.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.komozan.newsapp.R
 import com.komozan.newsapp.databinding.FragmentNewsContentBinding
+import com.komozan.newsapp.presentation.activity.MainActivity
+import com.komozan.newsapp.presentation.viewmodel.NewsViewModel
 
 class NewsContentFragment : Fragment() {
     private lateinit var binding: FragmentNewsContentBinding
+    private lateinit var viewModel: NewsViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,14 +27,53 @@ class NewsContentFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentNewsContentBinding.bind(view)
+        viewModel = (activity as MainActivity).viewModel
         val args: NewsContentFragmentArgs by navArgs()
         val article = args.selectedArticle
+        showProgressBar()
         binding.webView.apply {
-            webViewClient = WebViewClient()
+            webViewClient = MyWebViewClient()
             if (!TextUtils.isEmpty(article.url)) {
-                loadUrl(article.url)
+                loadUrl(article.url!!)
+                viewModel.saveArticle(article)
             }
         }
+    }
+
+    inner class MyWebViewClient : WebViewClient() {
+        override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+            view?.loadUrl(url!!)
+            return false
+        }
+
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+            hideProgressBar()
+        }
+
+        override fun onReceivedError(
+            view: WebView?,
+            request: WebResourceRequest?,
+            error: WebResourceError?
+        ) {
+            super.onReceivedError(view, request, error)
+        }
+
+        override fun onReceivedSslError(
+            view: WebView?,
+            handler: SslErrorHandler?,
+            error: SslError?
+        ) {
+            handler?.proceed()
+        }
+    }
+
+    private fun showProgressBar() {
+        binding.progressBar.visibility = View.VISIBLE
+    }
+
+    private fun hideProgressBar() {
+        binding.progressBar.visibility = View.INVISIBLE
     }
 
 }
--- app/src/main/java/com/komozan/newsapp/presentation/viewmodel/NewsViewModelFactory.kt
+++ app/src/main/java/com/komozan/newsapp/presentation/viewmodel/NewsViewModelFactory.kt
@@ -4,12 +4,21 @@ import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.komozan.newsapp.domain.usecase.GetNewsHeadlineUseCase
+import com.komozan.newsapp.domain.usecase.GetSpecifiedNewsAgencyUseCase
+import com.komozan.newsapp.domain.usecase.SaveNewsUseCase
 
 class NewsViewModelFactory(
     private val application: Application,
-    private val getNewsHeadlineUseCase: GetNewsHeadlineUseCase
+    private val getNewsHeadlineUseCase: GetNewsHeadlineUseCase,
+    private val getSpecifiedNewsAgencyUseCase: GetSpecifiedNewsAgencyUseCase,
+    private val saveNewsUseCase: SaveNewsUseCase
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return NewsViewModel(application, getNewsHeadlineUseCase) as T
+        return NewsViewModel(
+            application,
+            getNewsHeadlineUseCase,
+            getSpecifiedNewsAgencyUseCase,
+            saveNewsUseCase
+        ) as T
     }
 }
