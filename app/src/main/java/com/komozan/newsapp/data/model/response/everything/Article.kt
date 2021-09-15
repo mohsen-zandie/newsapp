64a41e88f17c727e1639e1cea24f272111fa2be4
mohsen <mohsen.zandie1@gmail.com>
2021-09-15 18:01:36 +0430

    Adds article data base, article dao and room dependencies

--- app/src/main/java/com/komozan/newsapp/data/model/response/everything/Article.kt
+++ app/src/main/java/com/komozan/newsapp/data/model/response/everything/Article.kt
@@ -1,14 +1,19 @@
 package com.komozan.newsapp.data.model.response.everything
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.io.Serializable
 
+@Entity(tableName = "articles")
 data class Article(
-    val author: String,
-    val content: String,
-    val description: String,
-    val publishedAt: String,
-    val source: Source,
-    val title: String,
-    val url: String,
-    val urlToImage: String
+    @PrimaryKey(autoGenerate = true)
+    val id : Int? = null,
+    val author: String?,
+    val content: String?,
+    val description: String?,
+    val publishedAt: String?,
+    val source: Source?,
+    val title: String?,
+    val url: String?,
+    val urlToImage: String?
 ) : Serializable
